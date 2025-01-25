b4b9280c552676679568d650d83401d8a79da823
end0rfin <bludovcom@yandex.ru>
2025-01-25 12:54:40 +0300

    feat(components): add login, dashboard, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ru } from "date-fns/locale";
+import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+
 function App() {
-  return <div>app</div>;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ru}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </LocalizationProvider>
+  );
 }
 
 export default App;
